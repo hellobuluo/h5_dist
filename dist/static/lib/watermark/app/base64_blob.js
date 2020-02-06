9ee6510a8aa7ddeda3d8e56f1016a95bf20b1973
叶国伟 <244935560@qq.com>
2020-02-06 20:21:22 +0800

    oss图片上传

---
+++ dist/static/lib/watermark/app/base64_blob.js
@@ -0,0 +1,17 @@
+define(function (dataurl) {
+  'use strict'
+  var base642blob = function (dataurl) {
+    var arr = dataurl.split(',')
+    var mime = arr[0].match(/:(.*?);/)[1]
+    var bstr = atob(arr[1])
+    var n = bstr.length
+    var u8arr = new Uint8Array(n)
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n)
+    }
+    return new Blob([u8arr], {
+      type: mime
+    })
+  }
+  return base642blob
+})
